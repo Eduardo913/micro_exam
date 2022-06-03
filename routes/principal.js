ef1e9fccd714ab3ece767a54c5605f54dac6602f
Eduardo913 <cesarvilla903@gmail.com>
2022-06-03 01:14:32 -0500

    first commit

---
+++ routes/principal.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const json = require('../resources/musica.json')
+
+router.post('/principal', function (req, res, next) {
+    res.json(json).sendStatus(200);
+  
+  });
+  
+module.exports = router;
+  
