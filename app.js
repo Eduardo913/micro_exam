e85a56905ad185b64bee7ea44eb10e1cf86914a2
Eduardo913 <cesarvilla903@gmail.com>
2022-06-03 09:55:52 -0500

    cors

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 const routePrincipal = require('./routes/principal')
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 app.use("/ceoo",routePrincipal)
 
